73878fabe702bdae401ae4e7add1cc18d1a9c62d
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-08-12 23:05:13 +0530

    Added

--- Methods/Main.java
+++ Methods/Main.java
@@ -2,15 +2,15 @@
  * Method without Return Type and Without Argumnets
  * In these Type of methods return Type wull be void and method will not have arguments
  */
-class Main{
+// class Main{
 
-    public static void main(String[]args){
-        System.out.println("Hello");
-run();
-    }
+//     public static void main(String[]args){
+//         System.out.println("Hello");
+// run();
+//     }
 
-    public static void run(){
-        System.out.println("This is method without return and without Argument");
+//     public static void run(){
+//         System.out.println("This is method without return and without Argument");
 
-    }
-}
+//     }
+// }
