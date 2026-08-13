858f7507f89a5a89c83eed07a5d1ea1c0819b070
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-08-13 18:14:25 +0530

    add

--- pattern/StarPattern/DiamondPattern.java
+++ pattern/StarPattern/DiamondPattern.java
@@ -3,7 +3,6 @@ package StarPattern;
 public class DiamondPattern {
     public static void main(String[] args) {
 
-
         
 //         * 
 //       * * * 
@@ -13,25 +12,27 @@ public class DiamondPattern {
 //   * * * * * * * 
 //     * * * * * 
 //       * * * 
-//         * 
+//         *
+
         int space = 4;
         int star = 1;
         for (int i = 1; i <= 9; i++) {
-
+            // Take a Loop to print spaces
             for (int j = 1; j <= space; j++) {
                 System.out.print("  ");
             }
 
+            // take a loop to print stars
             for (int a = 1; a <= star; a++) {
                 System.out.print("* ");
             }
 
             if (i <= 4) {
                 space--;
-                star+=2;
+                star += 2;
             } else {
                 space++;
-                star-=2;
+                star -= 2;
             }
             System.out.println();
         }
