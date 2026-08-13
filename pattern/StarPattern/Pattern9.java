5ed3073d786d6a6d6902eced63485e16eb3f7df9
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-08-13 16:55:10 +0530

    ADD

---
+++ pattern/StarPattern/Pattern9.java
@@ -0,0 +1,23 @@
+package StarPattern;
+
+//           *
+//        *
+//     *
+// * 
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        for(int i =1; i<=5; i++){
+            for(int j =1; j<=5; j++){
+                if(i+j==6)
+                    {
+                    System.out.println(" * ");
+
+                } else{
+                    System.out.print("    ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
