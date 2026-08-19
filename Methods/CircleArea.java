d75d320debb14f280080ca58759bb20e83eb10e9
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-08-19 11:25:59 +0530

    new

--- Methods/CircleArea.java
+++ Methods/CircleArea.java
@@ -1,3 +1,14 @@
+import java.util.Scanner ;
+
+
 public class CircleArea {
-    
+    public static  double findArea(int redius){
+        return 3.14 * redius * redius;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int redius = sc.nextInt();
+        System.out.println(findArea(redius));
+    }
 }
